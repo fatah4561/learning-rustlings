b5e7a1ef9eefc17628a7148dc2ef403016ad6aaa
fatah4561 <fatahathariq6@gmail.com>
2024-09-12 16:06:29 +0700

    remove folder

---
+++ target/debug/deps/learning_rustlings-8738e0a83155756e.d
@@ -0,0 +1,5 @@
+/Users/fatah/Documents/private/learning-rustlings/target/debug/deps/learning_rustlings-8738e0a83155756e: src/main.rs
+
+/Users/fatah/Documents/private/learning-rustlings/target/debug/deps/learning_rustlings-8738e0a83155756e.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/learning-rustlings.d
@@ -0,0 +1,1 @@
+/Users/fatah/Documents/private/learning-rustlings/target/debug/learning-rustlings: /Users/fatah/Documents/private/learning-rustlings/src/main.rs
